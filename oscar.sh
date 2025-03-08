80a97c7435284713ea88ef9cc87eb6e5b1a47430
Maciej Witkowiak <ytm@elysium.pl>
2025-03-08 21:37:15 +0100

    local tool path

--- oscar.sh
+++ oscar.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/home/maciej/Projekty-local/espllm/oscar64/bin/oscar64 -e test-tokenizer64.c \
+/home/maciej/Projekty/espllm/oscar64/bin/oscar64 -e test-tokenizer64.c \
  && x64 -reu -reusize 2048 -reuimage weights.bin test-transformer64.prg
 
