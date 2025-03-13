3873d7f7dd27c04af3c963856ee3dc3cf21c6e02
Maciej Witkowiak <ytm@elysium.pl>
2025-03-14 00:29:26 +0100

    default optimizations

--- oscar-generate64.sh
+++ oscar-generate64.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/home/maciej/Projekty/espllm/oscar64/bin/oscar64 -O0 test-generate64.c \
+/home/maciej/Projekty/espllm/oscar64/bin/oscar64 test-generate64.c \
  && x64 -reu -reusize 2048 -reuimage weights.bin test-generate64.prg
 
 # ./run stories260K.bin -z tok512.bin -i "Once upon a time" -t 0 -n 16
