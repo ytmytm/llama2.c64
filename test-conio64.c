2676ad7277bc232a60db4000217a8237a3f99776
Maciej Witkowiak <ytm@elysium.pl>
2025-03-20 00:46:30 +0100

    CIA TOD example

--- test-conio64.c
+++ test-conio64.c
@@ -1,9 +1,41 @@
 
+#include <c64/cia.h>
 #include <conio.h>
 #include <c64/charwin.h>
+#include <stdio.h>
 #include <stdint.h>
 
 //    https://github.com/drmortalwombat/oscar64/blob/main/include/c64/charwin.h
+
+void putbcd(uint8_t bcd) {
+    putch((bcd >> 4)   + '0');
+    putch((bcd & 0x0f) + '0');
+}
+
+void clock_init(void) {
+    #define palflag (*((uint8_t *)0x2A6))
+    uint8_t tv_mode = palflag << 7;
+
+    // setup clock
+    cia2.cra = (cia2.cra & 0x7f ) | tv_mode;
+    cia2.todh = 0;
+    cia2.todm = 0;
+    cia2.tods = 0;
+    cia2.todt = 0;
+    // force read to start clock
+    volatile uint8_t todt = cia2.todt;
+
+}
+
+void clock_display(void) {
+    char x = wherex();
+    char y = wherey();
+    gotoxy(40-8, 0);
+    putbcd(cia2.todh); putch(':'); putbcd(cia2.todm); putch(':'); putbcd(cia2.tods);
+    // force read to resume register update
+    volatile uint8_t todt = cia2.todt;
+    gotoxy(x, y);
+}
 
 int main(void) {
 
@@ -48,4 +80,9 @@ int main(void) {
         cwin_put_string(&bot, "PROMPT CANCELLED! ",3);
     }
 
+    clock_init();
+    while (1) {
+        clock_display();
+        if (getchx()) break;
+    }
 }
